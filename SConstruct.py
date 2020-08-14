2f4c1ee661cc666eebdbdbbe05f1ed4b7a5d3436
Nuckal777 <nuckal777@gmail.com>
2020-08-14 14:10:37 +0200

    cleanup

--- SConstruct.py
+++
@@ -1,8 +0,0 @@
-import os
-env = Environment(ENV = os.environ)
-# Use xelatex instead of pdfTeX.
-env.Replace(PDFLATEX='xelatex')
-# Use Biber instead of BiBTeX.
-env.Replace(BIBTEX='biber')
-env.AppendUnique(PDFLATEXFLAGS='-synctex=1')
-env.PDF(target = 'document.pdf', source = 'document.tex')
